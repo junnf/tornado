c4fb31b9c0ea0bb01fce25e11145284db5a521f8
junn <junnflow@gmail.com>
2015-06-24 02:09:56 +0800

    a

---
+++ projectSys/man.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#test ospath to import sometemplate
+
+import os.path
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+#define parameter
+from tornado.options import define, options
+define("port", default=8111, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+      #greeting = self.get_argument('greeting', 'Hello')
+      #self.write(greeting + ', friendly user!')
+      
+      #test index.html
+      self.render('index.html')
+
+class AllMessageHandler(tornado.web.RequestHandler):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def head(self):
+        pass
+ 
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler),
+                                            (r"/fridge/current",AllMessageHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
